f86b4b6002b8b926f4fda851edd39070566574de
stefanie <stefanie.rincones@gmail.com>
2024-06-27 18:16:03 -0400

    nombre del cambio

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3000
+const PORT = 3001
 const app = express()
 
 // configs
@@ -9,6 +9,29 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views/pages'))
 app.use(express.static('public'));
+
+
+app.get("/", (req, res)=> {
+    res.render("home", {titulo: "inicio"});
+});
+
+app.get("/Representante", (req, res)=>{
+    res.render("Representante")
+
+});
+
+app.get("/RegistrarseRepresentante", (req, res)=>{
+    res.render("RegistrarseRepresentante")
+
+});
+
+app.get("/Docente", (req, res)=>{
+      res.render("Docente")
+});
+
+app.get("/Coordinador", (req, res)=>{
+    res.render("Coordinador")
+});
 
 // routers
 app.use('/', require("./routers/routersPrueba"))
